99cad37ce0bb5931265e637a923c1438ede9e607
Sarthak <sarthak.vishwakarma.eee21@itbhu.ac.in>
2023-06-01 19:18:21 +0530

    added frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <HomeScreen />
+    </>
   )
 }
