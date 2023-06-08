49accdff4dd209e2eddee0dca4ecd744702ede4c
Sarthak <sarthak.vishwakarma.eee21@itbhu.ac.in>
2023-06-08 17:36:41 +0530

    added contest frontend

--- frontend/src/screens/ContestScreen.jsx
+++ frontend/src/screens/ContestScreen.jsx
@@ -1,30 +1,69 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Contest() {
+  const [myData, setMyData] = useState([]);
 
   async function loadContest() {
     const response = await axios.get("https://kontests.net/api/v1/all");
 
     const data = response.data.filter(contest => {
-      if (contest.site === "CodeForces" || contest.site === "CodeChef" || contest.site === "Atcoder") {
+      if (contest.site === "CodeForces" || contest.site === "CodeChef") {
         return contest;
       }
     }).map(contest => {
       return {
         name: contest.name,
-        url: contest.url,
-        start_time: contest.start_time,
-        end_time: contest.end_time,
-        site: contest.site
+        platform: contest.site,
+        startTime: contest.start_time,
+        endTime: contest.end_time,
+        url: contest.url
       }
     });
 
-    console.log(data);
+    setMyData(data);
   }
 
-  loadContest();
+  useEffect(() => {
+    loadContest();
+  }, [])
+
+  useEffect(() => {
+    console.log(myData);
+  }, [myData])
+
   return (
-    <div>Contest</div>
+    <Container>
+      <Row className="justify-content-center mt-5">
+        <Col>
+          <Table striped bordered hover className="text-center">
+            <thead>
+              <tr className="text-center">
+                <th>CONTEST</th>
+                <th>PLATFORM</th>
+                <th>START TIME</th>
+                <th>END TIME</th>
+                <th>LINK</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                myData.map(({ name, platform, startTime, endTime, url }) =>
+                  <tr>
+                    <td>{name}</td>
+                    <td>{platform}</td>
+                    <td>{startTime}</td>
+                    <td>{endTime}</td>
+                    <td><Link to={url} target="_blank" rel="noopener noreferrer">link</Link></td>
+                  </tr>
+                )
+              }
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   )
 }
